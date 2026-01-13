c3117ef480ae9ace65faddc00a8d0cee4d33212c
Naftali Horviz <n0548498093@gmail.com>
2026-01-13 16:30:33 +0200

    fixed try & except

--- service-b/services.py
+++ service-b/services.py
@@ -1,47 +1,37 @@
-from redis import Redis
-from fastapi import HTTPException, status
+from redis import Redis, ResponseError
 
 
-def get_coordinates(conn: Redis) -> dict:
+async def get_coordinates(conn: Redis) -> dict:
     try:
         data = {}
         keys = conn.keys("coord:*")
         if not keys:
             return {}
         for key in keys:
-            data[key] = conn.hgetall(key)
+            data[key] = await conn.hgetall(key)
 
         return {"data":data}
-    except HTTPException:
-        raise
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error retrieving data: {str(e)}"
-        )
+    except ResponseError as e:
+        raise e
 
 
-def get_coordinate(ip: str, conn: Redis) -> dict:
+async def get_coordinate(ip: str, conn: Redis) -> dict:
     key = f"coord:{ip}"
     try:
-        data = conn.hgetall(key)
+        data = await conn.hgetall(key)
         if not data:
-            raise HTTPException(status_code=404, detail="IP address not found")
-        return {"data": data}
-    except Exception:
-        raise HTTPException(status_code=500, detail="Database error")
+            return []
+    except ResponseError as e:
+        raise e
 
 
-def create_coordinate(coordinate: dict, conn: Redis):
+async def create_coordinate(coordinate: dict, conn: Redis):
     key = f"coord:{coordinate['ip']}"
     try:
-        conn.hset(key, mapping={"lat": coordinate["lat"], "lon": coordinate["lon"]})
+        await conn.hset(key, mapping={"lat": coordinate["lat"], "lon": coordinate["lon"]})
         return {
             "message": "The coordinates were saved successfully",
             "coordinate": coordinate
         }
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Saving to the database failed"
-        )   
+    except ResponseError as e:
+        raise e 
